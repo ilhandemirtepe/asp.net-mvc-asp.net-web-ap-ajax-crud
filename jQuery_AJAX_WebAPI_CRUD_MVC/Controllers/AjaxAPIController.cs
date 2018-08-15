393f24d8658e96db75e6b84c090fe6b4bdd6cf62
DESKTOP-42E6ERP\ilhandemirtepe <ilhandemirtepe@gmail.com>
2018-08-15 15:40:31 +0300

    asp.net mvc,web apı,ajax ,entityframewok crud
    asp.net mvc,web apı,ajax ,entityframewok crud

---
+++ jQuery_AJAX_WebAPI_CRUD_MVC/Controllers/AjaxAPIController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Web.Http;
+using System.Linq;
+
+namespace jQuery_AJAX_WebAPI_CRUD_MVC.Controllers
+{
+    public class AjaxAPIController : ApiController
+    {
+        [Route("api/AjaxAPI/InsertCustomer")]
+        [HttpPost]
+        public Customer InsertCustomer(Customer _customer)
+        {
+            using (TurkeyDBEntities entities = new TurkeyDBEntities())
+            {
+                entities.Customer.Add(_customer);
+                entities.SaveChanges();
+            }
+
+            return _customer;
+        }
+
+        [Route("api/AjaxAPI/UpdateCustomer")]
+        [HttpPost]
+        public bool UpdateCustomer(Customer _customer)
+        {
+            using (TurkeyDBEntities entities = new TurkeyDBEntities())
+            {
+                Customer updatedCustomer = (from c in entities.Customer
+                                            where c.CustomerId == _customer.CustomerId
+                                            select c).FirstOrDefault();
+                updatedCustomer.Name = _customer.Name;
+                updatedCustomer.Country = _customer.Country;
+                entities.SaveChanges();
+            }
+
+            return true;
+        }
+
+        [Route("api/AjaxAPI/DeleteCustomer")]
+        [HttpPost]
+        public void DeleteCustomer(Customer _customer)
+        {
+            using (TurkeyDBEntities entities = new TurkeyDBEntities())
+            {
+                Customer customer = (from c in entities.Customer
+                                     where c.CustomerId == _customer.CustomerId
+                                     select c).FirstOrDefault();
+                entities.Customer.Remove(customer);
+                entities.SaveChanges();
+            }
+        }
+    }
+}
---
+++ jQuery_AJAX_WebAPI_CRUD_MVC/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace jQuery_AJAX_WebAPI_CRUD_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            TurkeyDBEntities entities = new TurkeyDBEntities();
+            List<Customer> customers = entities.Customer.ToList();
+            if (customers.Count == 0)
+            {
+                customers.Add(new Customer());
+            }
+
+            return View(customers);
+        }
+    }
+}
